fe72eeb183fc87b21dbb5379d015e4be21881b62
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-07-14 09:36:54 -0300

    feat: add login com o google

--- app/src/main/java/com/programou/shuffled/enter/EnterFragment.kt
+++ app/src/main/java/com/programou/shuffled/enter/EnterFragment.kt
@@ -1,20 +1,46 @@
 package com.programou.shuffled.enter
 
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.programou.shuffled.R
+import com.programou.shuffled.SplashScreen
 import com.programou.shuffled.databinding.FragmentEnterBinding
 import com.programou.shuffled.utils.hideKeyboard
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
 
-class EnterFragment: Fragment(R.layout.fragment_enter) {
+class EnterFragment : Fragment(R.layout.fragment_enter) {
+
     private lateinit var binding: FragmentEnterBinding
-//    private val viewModel: EnterViewModel by lazy {
-//        val enterUseCase = EnterAccountUseCase()
-//        EnterViewModel(enterUseCase)
-//    }
+    private lateinit var signInRequest: GoogleSignInClient
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,12 +48,23 @@ class EnterFragment: Fragment(R.layout.fragment_enter) {
         binding = FragmentEnterBinding.bind(view)
 
         configureActions()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        signInRequest = GoogleSignIn.getClient(requireContext(), gso)
     }
 
     private fun configureActions() {
-        with (binding) {
+        with(binding) {
             buttonEnter.setOnClickListener { onEnterActionHandler(it) }
-            cardGoogle.setOnClickListener { onGoogleActionHandler() }
+            cardGoogle.setOnClickListener {
+                lifecycleScope.launch {
+                    onGoogleActionHandler()
+                }
+            }
             buttonDontHaveAccount.setOnClickListener { onCreateAccountActionHandler() }
             binding.root.setOnClickListener { hideKeyboard() }
         }
@@ -37,7 +74,43 @@ class EnterFragment: Fragment(R.layout.fragment_enter) {
         hideKeyboard()
     }
 
-    private fun onGoogleActionHandler() = Log.i("::DEBUG::", "enter with google")
+    private fun onGoogleActionHandler() {
+        val intent = signInRequest.signInIntent
+        launcher.launch(intent)
+    }
+
+    private var launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account = task.result
+            if (account != null) {
+                val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+                auth.signInWithCredential(credentials)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(requireContext(), SplashScreen::class.java)
+                            startActivity(intent)
+                            requireActivity().finish()
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                task.exception.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        } else {
+            Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
 
     private fun onCreateAccountActionHandler() {
         val action = EnterFragmentDirections.actionEnterFragmentToRegisterFragment()
