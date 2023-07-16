b12144cb1175711fe9e566e21d69a0f87aaeb036
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-07-15 21:56:45 -0300

    feat: implement a more beauty cell item in list of decks

--- app/src/main/java/com/programou/shuffled/authenticated/deckList/DeckListFragment.kt
+++ app/src/main/java/com/programou/shuffled/authenticated/deckList/DeckListFragment.kt
@@ -8,12 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.programou.shuffled.R
 import com.programou.shuffled.authenticated.ItemViewData
 import com.programou.shuffled.authenticated.ItemViewHolder
 import com.programou.shuffled.authenticated.ListAdapter
 import com.programou.shuffled.databinding.FragmentDeckListBinding
 import com.programou.shuffled.databinding.ViewDeckListItemBinding
+import com.programou.shuffled.databinding.ViewFavoriteDeckListItemBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -23,6 +25,7 @@ class DeckListFragment : Fragment(R.layout.fragment_deck_list) {
 
     private lateinit var binding: FragmentDeckListBinding
     private val deckListAdapter = ListAdapter<DeckViewData>()
+    private val recentDeckListAdapter = ListAdapter<DeckViewData>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,11 +39,27 @@ class DeckListFragment : Fragment(R.layout.fragment_deck_list) {
             }
         }
 
+        recentDeckListAdapter.register(DeckItemViewHolder.IDENTIFIER) { parent ->
+            FavoriteDeckItemViewHolder.instantiate(parent) { deckViewDataClicked ->
+                val action = DeckListFragmentDirections.actionDecksFragmentToDeckFragment()
+                findNavController().navigate(action)
+            }
+        }
+
         binding.progressDeckListLoader.visibility = View.VISIBLE
+
+        binding.textDecksTitle.visibility = View.GONE
+        binding.textRecentsDecks.visibility = View.GONE
+
+        binding.buttonCreateDeck.visibility = View.GONE
 
         binding.recyclerDecks.visibility = View.GONE
         binding.recyclerDecks.adapter = deckListAdapter
         binding.recyclerDecks.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        binding.recyclerDecksRecents.visibility = View.GONE
+        binding.recyclerDecksRecents.adapter = recentDeckListAdapter
+        binding.recyclerDecksRecents.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
         lifecycleScope.launch {
             updateMock()
@@ -62,8 +81,17 @@ class DeckListFragment : Fragment(R.layout.fragment_deck_list) {
 
             withContext(Dispatchers.Main) {
                 deckListAdapter.update(elements)
+                recentDeckListAdapter.update(elements)
+
                 binding.progressDeckListLoader.visibility = View.GONE
+
+                binding.buttonCreateDeck.visibility = View.VISIBLE
+
+                binding.textDecksTitle.visibility = View.VISIBLE
+                binding.textRecentsDecks.visibility = View.VISIBLE
+
                 binding.recyclerDecks.visibility = View.VISIBLE
+                binding.recyclerDecksRecents.visibility = View.VISIBLE
             }
         }
     }
@@ -88,9 +116,30 @@ class DeckItemViewHolder private constructor(private val binding: ViewDeckListIt
     }
 
     override fun bind(viewData: DeckViewData) {
-        binding.editDeckTitle.text = viewData.title
+        binding.textDeckTitle.text = viewData.title
         binding.cardContainer.setOnClickListener {
             onClick?.let{ it(viewData) }
         }
     }
 }
+
+class FavoriteDeckItemViewHolder private constructor(private val binding: ViewFavoriteDeckListItemBinding, private val onClick: Bind<DeckViewData>?): ItemViewHolder<DeckViewData>(binding.root) {
+    companion object {
+        val IDENTIFIER: Int by lazy { DeckItemViewHolder.hashCode() }
+
+        fun instantiate(parent: ViewGroup, onClick: Bind<DeckViewData>?): FavoriteDeckItemViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ViewFavoriteDeckListItemBinding.inflate(inflater, parent, false)
+
+            return FavoriteDeckItemViewHolder(binding, onClick)
+        }
+    }
+
+    override fun bind(viewData: DeckViewData) {
+        binding.textDeckTitle.text = viewData.title
+        binding.cardContainer.setOnClickListener {
+            onClick?.let{ it(viewData) }
+        }
+    }
+}
+
